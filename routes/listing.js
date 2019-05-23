3290f5389396872be795f413c44ab84113f00ac5
Jing Su <43655970+jingxiang4766@users.noreply.github.com>
2019-05-22 18:54:28 -0700

    delete

--- routes/listing.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var ride_list = require('../controller/ride_list'); // import the controller
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  ride_list.view_pending(req, res, next);
-});
-
-module.exports = router;
